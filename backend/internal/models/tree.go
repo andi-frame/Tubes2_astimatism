005f73804d8ae366755327ff7ce318686e3e45c1
andi-frame <andifarhan1094@gmail.com>
2025-05-06 19:29:32 +0700

    feat: edit TreeNode to only store parent, element, and children

--- backend/internal/models/tree.go
+++ backend/internal/models/tree.go
@@ -1,6 +1,7 @@
 package models
 
 type TreeNode struct {
-	Element   string     `json:"element"`
-	Children  []*TreeNode `json:"children,omitempty"`
+	Parent   *TreeNode   `json:"-"`
+	Element  string      `json:"element"`
+	Children []*TreeNode `json:"children,omitempty"`
 }
