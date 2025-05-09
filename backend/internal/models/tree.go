e5efc5446046847b765b15a8be4c7026671968f3
andi-frame <andifarhan1094@gmail.com>
2025-05-10 03:51:23 +0700

    feat: update node structs for bottom-up structure

--- backend/internal/models/tree.go
+++ backend/internal/models/tree.go
@@ -1,7 +1,21 @@
 package models
 
+// Top-Down Version: (unfinished)
+// type TreeNode struct {
+// 	Parent      *TreeNode   `json:"-"`
+// 	Element     string      `json:"element"`
+// 	Children    []*TreeNode `json:"children,omitempty"`
+// 	Clear       bool        `json:"-"`
+// 	IsGroupNode bool        `json:"-"`
+// }
+
 type TreeNode struct {
-	Parent   *TreeNode   `json:"-"`
-	Element  string      `json:"element"`
-	Children []*TreeNode `json:"children,omitempty"`
+	Elements []*RecipeNode `json:"elements"`
+	Children []*TreeNode   `json:"children"`
+}
+
+type RecipeNode struct {
+	Element     int         `json:"element"`
+	Ingredient1 *RecipeNode `json:"ingredient1"`
+	Ingredient2 *RecipeNode `json:"ingredient2"`
 }
