e545a64e4f4faa10f02c6e1a4e631e7ccf5c0b1c
andi-frame <andifarhan1094@gmail.com>
2025-05-10 03:48:48 +0700

    feat: update BuildGraph and IsBaseElement for id as element identifier and new bottom-up structure

--- backend/internal/logic/base_logic.go
+++ backend/internal/logic/base_logic.go
@@ -2,17 +2,30 @@ package logic
 
 import "github.com/andi-frame/Tubes2_astimatism/backend/internal/models"
 
-func GraphElements(recipes []models.RecipeType) map[string][]models.RecipeType {
-	result := make(map[string][]models.RecipeType)
+// Top-Down Version:
+// func GraphElements(recipes []models.RecipeType) map[string][]models.RecipeType {
+// 	result := make(map[string][]models.RecipeType)
+// 	for _, r := range recipes {
+// 		result[r.Element] = append(result[r.Element], r)
+// 	}
+// 	return result
+// }
+
+func BuildGraph(recipes []models.RecipeType) map[models.PairElement]int {
+	result := make(map[models.PairElement]int)
 	for _, r := range recipes {
-		result[r.Element] = append(result[r.Element], r)
+		pair := models.PairElement{
+			Element1: r.IngredientId1,
+			Element2: r.IngredientId2,
+		}
+		result[pair] = r.ElementId
 	}
 	return result
 }
 
-func IsBaseElement(name string) bool {
-	base := map[string]bool{
-		"Air": true, "Water": true, "Earth": true, "Fire": true,
+func IsBaseElement(id int) bool {
+	base := map[int]bool{
+		1: true, 2: true, 3: true, 4: true,
 	}
-	return base[name]
+	return base[id]
 }
