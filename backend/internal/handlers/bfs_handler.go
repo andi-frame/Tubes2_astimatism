b3bdf4281fd3554633307f1c9ba29cf44598a107
andi-frame <andifarhan1094@gmail.com>
2025-05-13 18:38:31 +0700

    feat: add unfinished bfs with websocket

--- backend/internal/handlers/bfs_handler.go
+++ backend/internal/handlers/bfs_handler.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 
 	"github.com/andi-frame/Tubes2_astimatism/backend/internal/logic"
 	"github.com/andi-frame/Tubes2_astimatism/backend/internal/models"
+	"github.com/coder/websocket"
 	"github.com/gin-gonic/gin"
 )
 
@@ -65,4 +67,50 @@ func LimitedBFSTree(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"data": result,
 	})
+}
+
+
+// For Live Update
+func WebsocketBFSTreeHandler(c *gin.Context) {
+	w := c.Writer
+	r := c.Request
+
+	socket, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		log.Printf("could not open websocket: %v", err)
+		http.Error(w, "could not open websocket", http.StatusInternalServerError)
+		return
+	}
+	defer socket.Close(websocket.StatusNormalClosure, "closing websocket")
+
+	ctx := r.Context()
+
+	targetId, err := strconv.Atoi(c.Query("target"))
+	if err != nil || targetId == 0 {
+		socket.Write(ctx, websocket.MessageText, []byte(`{"error":"Invalid or missing target"}`))
+		return
+	}
+
+	limit, err := strconv.ParseUint(c.Query("limit"), 10, 64)
+	if err != nil || limit == 0 {
+		socket.Write(ctx, websocket.MessageText, []byte(`{"error":"Invalid or missing limit"}`))
+		return
+	}
+
+	fileBytes, err := os.ReadFile("data/recipes.json")
+	if err != nil {
+		socket.Write(ctx, websocket.MessageText, []byte(`{"error":"Failed to read recipes file"}`))
+		return
+	}
+
+	var recipes []models.RecipeType
+	if err := json.Unmarshal(fileBytes, &recipes); err != nil {
+		socket.Write(ctx, websocket.MessageText, []byte(`{"error":"Failed to unmarshal recipes"}`))
+		return
+	}
+
+	graph := logic.BuildGraph(recipes)
+	tierMap := logic.BuildTierMap(recipes)
+
+	logic.StreamLimitedBFSTree(ctx, socket, targetId, graph, tierMap, limit)
 }
